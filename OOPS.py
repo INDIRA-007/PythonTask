ddfc5012adc70ec42fa2d703f8718cb24a123b19
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-10-02 11:18:34 +0530

    latest changes

---
+++ OOPS.py
@@ -0,0 +1,23 @@
+
+# internal Module
+# external Module
+# userdefined Module -> object
+
+
+#OOPS
+#object oriented programming 
+
+#variables
+#loop
+#operators
+
+#functions
+    #variables
+    #loop
+    #operators
+
+#class
+    #funtions
+    #objects
+
+# class Name:
